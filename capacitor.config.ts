2bd3de240e75e9e25e64c2223509fe822aa3e0f7
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-05-13 16:57:50 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TierheimTiere',
+  webDir: 'www'
+};
+
+export default config;
